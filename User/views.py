6c65b8c9ae99ce5b05a5b6bc1f54ba36dd7a2109
Merdan <babayevmerdan20@gmail.com>
2023-09-11 11:26:55 +0300

    Register & Login

--- User/views.py
+++ User/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 def LoginView(request):
+    if request.method == 'POST':
+        user=User()
+        form=request.POST
+        username=form.get('usrnm')
+        password=form.get('password')
+        user=authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, "Ulgama Girmani Başardynyz")
+            return redirect('index')
+        else:
+            messages.error(request, "Ulanyjy Ady yada Parolanyz Yalnyşdyr")
+            return redirect('login',)
+            
     return render(request, "login/login.html")
 
 def SiginView(request):
+    if request.method=="POST":
+        form=request.POST
+        username=form.get('usrnm')
+        email=form.get('email')
+        password=form.get('password')
+        passwordConf=form.get('passwordConf')
+        if( password != passwordConf):
+            messages.warning(request, 'Parolanyz we gaytadan giren Parolanyz menzeş dal')
+            return redirect('sigin')
+        user=User.objects.create_user(username=username, password=password)
+        if user:
+            user.save()
+            messages.success(request, 'Başaryly Agza Boldunuz. Ulanjy Ady ve Parolanyzy yazyp ulgama girip bilersiniz')
+            return redirect('login')
+        
     return render(request, "login/register.html")
